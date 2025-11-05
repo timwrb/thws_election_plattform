a4714bcfc229744861d95c587753ba3658643303
Tim Wurmbrand <wurmbrand.tim@gmail.com>
2025-11-05 21:37:55 +0100

    chore: basic app provider settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(app()->inProduction());
+        Model::unguard();
+        Vite::useAggressivePrefetching();
     }
 }
