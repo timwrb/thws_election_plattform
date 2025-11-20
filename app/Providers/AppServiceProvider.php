4cc0afc9ea246f0e823e3a36e94986b400fbd507
Daniel Horman <uhorman@gmail.com>
2025-11-20 15:30:27 +0100

    chore: cleanup AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,18 +10,10 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     #[\Override]
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        DB::prohibitDestructiveCommands(app()->environment('production'));
-        Model::unguard();
-        Vite::useAggressivePrefetching();
-    }
+    public function boot(): void {}
 }
