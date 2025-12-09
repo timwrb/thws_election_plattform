a66bdf570f4ac6bc9b02d4d8a97cb828ddf37b0a
Daniel Horman <uhorman@gmail.com>
2025-12-09 21:00:55 +0000

    chore: use Spatie roles and permissions package for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,14 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    use HasRoles;
 
     /**
      * The attributes that should be hidden for serialization.
