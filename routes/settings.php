94affeaeaa10f735e278195e554b957420577c5e
Daniel Horman <uhorman@gmail.com>
2025-12-09 21:01:32 +0000

    rm dead code

--- routes/settings.php
+++ routes/settings.php
@@ -1,22 +1,5 @@
 <?php
 
-use App\Http\Controllers\Settings\PasswordController;
-use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::middleware('auth')->group(function (): void {
-    Route::redirect('settings', '/settings/profile');
-
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('settings/password', [PasswordController::class, 'edit'])->name('user-password.edit');
-
-    Route::put('settings/password', [PasswordController::class, 'update'])
-        ->middleware('throttle:6,1')
-        ->name('user-password.update');
-
-    Route::get('settings/appearance', fn () => Inertia::render('settings/Appearance'))->name('appearance.edit');
-});
+Route::middleware('auth')->group(function (): void {});
