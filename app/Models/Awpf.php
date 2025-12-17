eb1f70a7c9e2a11d8fce4ed88ce85ad52cf1d5be
Tim Wurmbrand <wurmbrand.tim@gmail.com>
2025-12-17 20:48:01 +0100

    chore: add trait

--- app/Models/Awpf.php
+++ app/Models/Awpf.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasOrderedUserChoices;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,5 @@ class Awpf extends Model
 {
     /** @use HasFactory<\Database\Factories\AwpfFactory> */
     use HasFactory;
+    use HasOrderedUserChoices;
 }
--- app/Models/Fwpm.php
+++ app/Models/Fwpm.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasOrderedUserChoices;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,5 @@ class Fwpm extends Model
 {
     /** @use HasFactory<\Database\Factories\FwpmFactory> */
     use HasFactory;
+    use HasOrderedUserChoices;
 }
