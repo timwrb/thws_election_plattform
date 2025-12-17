5938eec7cb8676f822d97e7dc481035a83e50edd
Daniel Horman <uhorman@gmail.com>
2025-12-17 19:44:15 +0100

    wip

--- database/migrations/2025_12_09_204740_create_awpfs_table.php
+++ database/migrations/2025_12_09_204740_create_awpfs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Language;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('awpfs', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('content')->nullable();
+
+            $table->integer('credits')->default(5);
+            $table->enum(Language::class);
+
             $table->timestamps();
         });
     }
