6439cea5972b8cc318176c3d882efdbfa7fa1dde
Daniel Horman <uhorman@gmail.com>
2026-01-11 18:24:22 +0100

    chore(wip): add deps, filament shield

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\ElectivesPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
 ];
