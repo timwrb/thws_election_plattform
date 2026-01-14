a31682941a7cb7eccfc0479e9b55caf31cd72e29
Daniel Horman <uhorman@gmail.com>
2026-01-14 22:43:26 +0100

    chore: add research project factory

--- database/factories/ResearchProjectFactory.php
+++ database/factories/ResearchProjectFactory.php
@@ -17,7 +17,13 @@ class ResearchProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'supervisor' => fake()->name(),
+            'credits' => fake()->numberBetween(5, 10),
+            'max_students' => fake()->numberBetween(1, 5),
+            'start_date' => null,
+            'end_date' => null,
         ];
     }
 }
