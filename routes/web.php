e1323bc15a1bf21f9932b038574b3b9e6b427ffd
Tim Wurmbrand <wurmbrand.tim@gmail.com>
2026-01-14 19:03:18 +0100

    feat: perm redirect '/' to electives panel

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
-
-Route::get('/', fn () => Inertia::render('Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-]))->name('home');
-
-Route::get('dashboard', fn () => Inertia::render('Dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
+Route::permanentRedirect('/', '/electives')->name('home');
