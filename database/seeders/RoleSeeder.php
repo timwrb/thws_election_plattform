534626a0cdcbe8ca062043190a6f7eeb74fe5428
Daniel Horman <uhorman@gmail.com>
2026-01-17 15:14:39 +0100

    refactor: role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -9,28 +9,26 @@ use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
 
-class SuperAdminSeeder extends Seeder
+class RoleSeeder extends Seeder
 {
     /**
      * Seed super admin users who can access and configure everything.
      */
     public function run(): void
     {
-        // Generate Shield permissions for all resources
         Artisan::call('shield:generate', [
             '--all' => true,
             '--panel' => 'admin',
             '--option' => 'permissions',
         ]);
-
-        // Reset permission cache
-        app(PermissionRegistrar::class)->forgetCachedPermissions();
+        resolve(PermissionRegistrar::class)->forgetCachedPermissions();
 
         $superAdminRole = Role::firstOrCreate(['name' => 'super_admin', 'guard_name' => 'web']);
+        $professorRole = Role::firstOrCreate(['name' => 'professor', 'guard_name' => 'web']);
         $studentRole = Role::firstOrCreate(['name' => 'student', 'guard_name' => 'web']);
 
-        // Assign all permissions to super_admin role
         $superAdminRole->syncPermissions(Permission::all());
+        $professorRole->syncPermissions(Permission::all());
 
         $daniel = User::query()->firstOrCreate(
             ['email' => 'daniel@thws.de'],
@@ -40,7 +38,7 @@ class SuperAdminSeeder extends Seeder
                 'password' => 'password',
             ]
         );
-        $daniel->syncRoles([$superAdminRole, $studentRole]);
+        $daniel->syncRoles([$superAdminRole, $studentRole, $professorRole]);
 
         $tim = User::query()->firstOrCreate(
             ['email' => 'tim@thws.de'],
@@ -50,6 +48,6 @@ class SuperAdminSeeder extends Seeder
                 'password' => 'password',
             ]
         );
-        $tim->syncRoles([$superAdminRole, $studentRole]);
+        $tim->syncRoles([$superAdminRole, $studentRole, $professorRole]);
     }
 }
