4de3c07bbfc481e971c35781471f9288047dfbc5
Daniel Horman <uhorman@gmail.com>
2026-01-18 13:36:42 +0100

    chore: configure rector-laravel rules

--- rector.php
+++ rector.php
@@ -3,6 +3,26 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\Class_\LivewireComponentComputedMethodToComputedAttributeRector;
+use RectorLaravel\Rector\Class_\LivewireComponentQueryStringToUrlAttributeRector;
+use RectorLaravel\Rector\Class_\RemoveModelPropertyFromFactoriesRector;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use RectorLaravel\Rector\Coalesce\ApplyDefaultInsteadOfNullCoalesceRector;
+use RectorLaravel\Rector\Empty_\EmptyToBlankAndFilledFuncRector;
+use RectorLaravel\Rector\FuncCall\FactoryFuncCallToStaticCallRector;
+use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
+use RectorLaravel\Rector\FuncCall\TypeHintTappableCallRector;
+use RectorLaravel\Rector\MethodCall\AssertStatusToAssertMethodRector;
+use RectorLaravel\Rector\MethodCall\EloquentOrderByToLatestOrOldestRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereRelationTypeHintingParameterRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereTypeHintClosureParameterRector;
+use RectorLaravel\Rector\MethodCall\FactoryApplyingStatesRector;
+use RectorLaravel\Rector\MethodCall\ResponseHelperCallToJsonResponseRector;
+use RectorLaravel\Rector\MethodCall\WhereToWhereLikeRector;
+use RectorLaravel\Rector\PropertyFetch\OptionalToNullsafeOperatorRector;
+use RectorLaravel\Rector\StaticCall\CarbonSetTestNowToTravelToRector;
+use RectorLaravel\Rector\StaticCall\EloquentMagicMethodToQueryBuilderRector;
+use RectorLaravel\Rector\StaticCall\RouteActionCallableRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,11 +34,41 @@ return RectorConfig::configure()
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
+    ->withPhpSets()
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
-        typeDeclarations: true,
         earlyReturn: true,
     )
-    // uncomment to reach your current PHP version
-    ->withPhpSets();
+    ->withSets([
+        RectorLaravel\Set\LaravelLevelSetList::UP_TO_LARAVEL_120,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_CODE_QUALITY,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_IF_HELPERS,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_COLLECTION,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+    ])
+    ->withRules(rules: [
+        AddGenericReturnTypeToRelationsRector::class,
+        ApplyDefaultInsteadOfNullCoalesceRector::class,
+        AssertStatusToAssertMethodRector::class,
+        CarbonSetTestNowToTravelToRector::class,
+        EloquentMagicMethodToQueryBuilderRector::class,
+        EloquentOrderByToLatestOrOldestRector::class,
+        EloquentWhereRelationTypeHintingParameterRector::class,
+        EloquentWhereTypeHintClosureParameterRector::class,
+        EmptyToBlankAndFilledFuncRector::class,
+        FactoryApplyingStatesRector::class,
+        FactoryFuncCallToStaticCallRector::class,
+        LivewireComponentComputedMethodToComputedAttributeRector::class,
+        LivewireComponentQueryStringToUrlAttributeRector::class,
+        OptionalToNullsafeOperatorRector::class,
+        RemoveDumpDataDeadCodeRector::class,
+        RemoveModelPropertyFromFactoriesRector::class,
+        ResponseHelperCallToJsonResponseRector::class,
+        RouteActionCallableRector::class,
+        TypeHintTappableCallRector::class,
+        WhereToWhereLikeRector::class,
+    ]);
